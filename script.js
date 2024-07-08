d2a1fe8deb27bbd89976e5c3315efc5508385e91
Thomas Riedel <t.23.riedel@gmail.com>
2024-07-08 23:39:25 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+function render() {
+      document.getElementById('menu-section').innerHTML = '';
+
+      for (let sectionIndex = 0; sectionIndex < menu.length; sectionIndex++) {
+            const section = menu[sectionIndex];
+            getSavedArrays(section, sectionIndex);
+            document.getElementById('menu-section').innerHTML += generateMenuSectionHTML(section, sectionIndex);
+            const dishArea = document.getElementById(`${sectionIndex}-dish-area`);
+            dishArea.innerHTML = '';
+            for (let dishIndex = 0; dishIndex < section.dishes.length; dishIndex++) {
+                  const dish = section.dishes[dishIndex];
+                  dishArea.innerHTML += generateDishAreaHTML(section, sectionIndex, dish, dishArea, dishIndex);
+            }
+      }
+}
+
+function getSavedArrays() {}
---
+++ scripts/templates.js
@@ -0,0 +1,20 @@
+function generateMenuSectionHTML(section, sectionIndex) {
+      return /*html*/ `
+        <h2>${section.section}</h2>
+        <h6>${section.description}</h6>
+        <div id="${sectionIndex}-dish-area">
+        </div>
+    `;
+}
+
+function generateDishAreaHTML(section, sectionIndex, dish, dishArea, dishIndex) {
+      return /*html*/ `
+        <div class="dish">
+            <div class="name-button-container">
+                <h3>${dish.name}</h3><button class="add-dish-button">+</button>
+            </div>
+            <h5>${dish.ingredients}</h5>
+            <h4>${dish.price}€</h4>
+        </div>
+    `;
+}
